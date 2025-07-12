a91d7c6de3d59b8aeb956d8e0daca9475308f6a3
Nerbil <perry@perrywayman.com>
2025-07-12 21:41:54 +0100

    B: Allowed for user arguments to path to a book in .txt format

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 from stats import word_count
 from stats import character_count
 from stats import sorted_list
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    text = get_book_text(sys.argv[1])
     num_words = word_count(text)
     print(f"""
     ============ BOOKBOT ============
-    Analyzing book found at books/frankenstein.txt...
+    Analyzing book found at {sys.argv[1]}...
     ----------- Word Count ----------
     Found {num_words} total words""")
     character_list = character_count(text)
